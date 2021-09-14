108f53f47ecc0ae2541bba8b971b7a334940b700
sh.khalajestanii <NiH@sh123>
2021-09-14 12:09:54 +0430

    Adding BulkHead For Concurrent Calls

--- currency-exchange-service/src/main/java/com/shahinkhalajestani/currencyexchangeservice/controllers/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/shahinkhalajestani/currencyexchangeservice/controllers/CircuitBreakerController.java
@@ -1,6 +1,7 @@
 package com.shahinkhalajestani.currencyexchangeservice.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
@@ -20,7 +21,8 @@ public class CircuitBreakerController {
     @GetMapping("/sample-api")
 //    @Retry(name = "sample-api",fallbackMethod = "hardCodedResponse")
 //    @CircuitBreaker(name = "sample-api",fallbackMethod = "hardCodedResponse")
-    @RateLimiter(name="sample-api")
+//    @RateLimiter(name="sample-api")
+    @Bulkhead(name = "sample-api")
     public String sampleApi() {
         logger.info("sample api call recieved ");
 //        ResponseEntity<String> response = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
