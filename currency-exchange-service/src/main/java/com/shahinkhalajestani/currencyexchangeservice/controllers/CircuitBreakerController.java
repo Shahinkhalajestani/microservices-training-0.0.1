0a8a7a56c57b9cdad7ea6202473f01537209a688
sh.khalajestanii <NiH@sh123>
2021-09-14 12:04:31 +0430

    Adding RateLimiter

--- currency-exchange-service/src/main/java/com/shahinkhalajestani/currencyexchangeservice/controllers/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/shahinkhalajestani/currencyexchangeservice/controllers/CircuitBreakerController.java
@@ -2,6 +2,7 @@ package com.shahinkhalajestani.currencyexchangeservice.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,11 +19,13 @@ public class CircuitBreakerController {
 
     @GetMapping("/sample-api")
 //    @Retry(name = "sample-api",fallbackMethod = "hardCodedResponse")
-    @CircuitBreaker(name = "sample-api",fallbackMethod = "hardCodedResponse")
+//    @CircuitBreaker(name = "sample-api",fallbackMethod = "hardCodedResponse")
+    @RateLimiter(name="sample-api")
     public String sampleApi() {
         logger.info("sample api call recieved ");
-        ResponseEntity<String> response = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
-        return response.getBody();
+//        ResponseEntity<String> response = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
+//        return response.getBody();
+        return "Sample API !";
     }
 
     public String hardCodedResponse(Exception exception){
