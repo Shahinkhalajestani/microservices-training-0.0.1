88cd62d86f0a46617938b55e8614193c093a6a93
sh.khalajestanii <NiH@sh123>
2021-09-14 11:06:24 +0430

    Playing with resilience4j

--- currency-exchange-service/src/main/java/com/shahinkhalajestani/currencyexchangeservice/controllers/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/shahinkhalajestani/currencyexchangeservice/controllers/CircuitBreakerController.java
@@ -1,14 +1,30 @@
 package com.shahinkhalajestani.currencyexchangeservice.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CircuitBreakerController {
 
-    @GetMapping("/getSample")
-    public String sampleApi(){
-        return "Sample API";
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @GetMapping("/sample-api")
+    @Retry(name = "sample-api",fallbackMethod = "hardCodedResponse")
+    public String sampleApi() {
+        logger.info("sample api call recieved ");
+        ResponseEntity<String> response = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
+        return response.getBody();
+    }
+
+    public String hardCodedResponse(Exception exception){
+        return "failure for sample api ";
     }
 
 }
