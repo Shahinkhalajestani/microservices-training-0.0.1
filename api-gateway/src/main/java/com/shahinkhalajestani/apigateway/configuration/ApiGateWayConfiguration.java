4c7b876cdf4d2d1b13b9a96dfd6f51e885fc25a1
sh.khalajestanii <NiH@sh123>
2021-09-28 13:58:31 +0330

    Added ZipKin Support For Currency-Conversion And Api-Gateway .

--- api-gateway/src/main/java/com/shahinkhalajestani/apigateway/configuration/ApiGateWayConfiguration.java
+++ api-gateway/src/main/java/com/shahinkhalajestani/apigateway/configuration/ApiGateWayConfiguration.java
@@ -16,10 +16,11 @@ public class ApiGateWayConfiguration {
     @Bean
     public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route(p -> p.path("/get")
-                        .filters(f -> f.addRequestHeader("MyHeader", "MyUri")
-                                .addRequestParameter("Param", "MyValue"))
-                        .uri("http://httpbin.org:80"))
+                //idk what is the usage for this
+//                .route(p -> p.path("/get")
+//                        .filters(f -> f.addRequestHeader("MyHeader", "MyUri")
+//                                .addRequestParameter("Param", "MyValue"))
+//                        .uri("http://httpbin.org:80"))
                 .route(p -> p.path("/currency-exchange/**")
                         .uri("lb://currency-exchange"))
                 .route(p -> p.path("/currency-conversion/**")
